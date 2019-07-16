b0aaf26ce5427ababbe470895b480b86f77a4043
Kedar Vijay Kulkarni <kkulkarn@redhat.com>
2019-07-16 16:00:18 -0400

    Adding logs to checks

--- osp_checks.py
+++ osp_checks.py
@@ -9,19 +9,26 @@ import sys
 
 def check_vm_count(system, warn=10, crit=15, **kwargs):
     """ Check overall vm count. """
+    logger = kwargs["logger"]
     warn = int(warn)
     crit = int(crit)
     vm_count = len(system.list_vms())
     # determine ok, warning, critical, unknown state
     if vm_count < warn:
-        print("Ok: VM count is less than {}. VM Count = {}".format(warn, vm_count))
+        msg = ("Ok: VM count is less than {}. VM Count = {}".format(warn, vm_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(0)
     elif vm_count >= warn and vm_count <= crit:
-        print("Warning: VM count is >=  {} & <= {}. VM Count = {}"
+        msg = ("Warning: VM count is >=  {} & <= {}. VM Count = {}"
             .format(warn, crit, vm_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(1)
     elif vm_count > crit:
-        print("Critical: VM count is greate than crit. VM Count = {}".format(crit, vm_count))
+        msg = ("Critical: VM count is greater than {}. VM Count = {}".format(crit, vm_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(2)
     else:
         print("Unknown: VM count is unknown")
@@ -30,45 +37,63 @@ def check_vm_count(system, warn=10, crit=15, **kwargs):
 
 def check_keypair_count(system, warn=10, crit=15, **kwargs):
     """ Check overall keypair count. """
+    logger = kwargs["logger"]
     warn = int(warn)
     crit = int(crit)
     keypair_count = len(system.list_keypair())
     # determine ok, warning, critical, unknown state
     if keypair_count < warn:
-        print("Ok: Keypair count is less than {}. Keypair Count = {}".format(warn, keypair_count))
+        msg = "Ok: Keypair count is less than {}. Keypair Count = {}".format(warn, keypair_count)
+        logger.info(msg)
+        print(msg)
         sys.exit(0)
     elif keypair_count >= warn and keypair_count <= crit:
-        print("Warning: Keypair count is >=  {} & <= {}. Keypair Count = {}"
+        msg = ("Warning: Keypair count is >=  {} & <= {}. Keypair Count = {}"
             .format(warn, crit, keypair_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(1)
     elif keypair_count > crit:
-        print("Critical: Keypair count is greate than crit. Keypair Count = {}".format(
+        msg = ("Critical: Keypair count is greater than {}. Keypair Count = {}".format(
             crit, keypair_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(2)
     else:
-        print("Unknown: Keypair count is unknown")
+        msg = ("Unknown: Keypair count is unknown")
+        logger.info(msg)
+        print(msg)
     sys.exit(3)
 
 
 def check_volume_count(system, warn=10, crit=15, **kwargs):
     """ Check overall volume count. """
+    logger = kwargs["logger"]
     warn = int(warn)
     crit = int(crit)
     volume_count = len(system.list_volume())
     # determine ok, warning, critical, unknown state
     if volume_count < warn:
-        print("Ok: Volume count is less than {}. Volume Count = {}".format(warn, volume_count))
+        msg = ("Ok: Volume count is less than {}. Volume Count = {}".format(warn, volume_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(0)
     elif volume_count >= warn and volume_count <= crit:
-        print("Warning: Volume count is >=  {} & <= {}. Volume Count = {}"
+        msg = ("Warning: Volume count is >=  {} & <= {}. Volume Count = {}"
             .format(warn, crit, volume_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(1)
     elif volume_count > crit:
-        print("Critical: Volume count is greate than crit. Volume Count = {}".format(
+        msg = ("Critical: Volume count is greater than {}. Volume Count = {}".format(
             crit, volume_count))
+        logger.info(msg)
+        print(msg)
         sys.exit(2)
     else:
-        print("Unknown: Volume count is unknown")
+        msg = ("Unknown: Volume count is unknown")
+        logger.info(msg)
+        print(msg)
     sys.exit(3)
 
 
