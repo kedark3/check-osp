5993035cb4aae85da6b28e68cf3315075caf5bbd
Kedar Vijay Kulkarni <kkulkarn@redhat.com>
2019-07-17 13:54:20 -0400

    Adding OSP Services check

--- osp_checks.py
+++ osp_checks.py
@@ -6,6 +6,9 @@ OSP API/Wrapanapi
 
 import sys
 
+from utils import is_service_in_status
+from utils import ssh_client
+
 
 def check_vm_count(system, warn=10, crit=15, **kwargs):
     """ Check overall vm count. """
@@ -22,12 +25,12 @@ def check_vm_count(system, warn=10, crit=15, **kwargs):
     elif vm_count >= warn and vm_count <= crit:
         msg = ("Warning: VM count is >=  {} & <= {}. VM Count = {}"
             .format(warn, crit, vm_count))
-        logger.info(msg)
+        logger.warning(msg)
         print(msg)
         sys.exit(1)
     elif vm_count > crit:
         msg = ("Critical: VM count is greater than {}. VM Count = {}".format(crit, vm_count))
-        logger.info(msg)
+        logger.error(msg)
         print(msg)
         sys.exit(2)
     else:
@@ -50,13 +53,13 @@ def check_keypair_count(system, warn=10, crit=15, **kwargs):
     elif keypair_count >= warn and keypair_count <= crit:
         msg = ("Warning: Keypair count is >=  {} & <= {}. Keypair Count = {}"
             .format(warn, crit, keypair_count))
-        logger.info(msg)
+        logger.warning(msg)
         print(msg)
         sys.exit(1)
     elif keypair_count > crit:
         msg = ("Critical: Keypair count is greater than {}. Keypair Count = {}".format(
             crit, keypair_count))
-        logger.info(msg)
+        logger.error(msg)
         print(msg)
         sys.exit(2)
     else:
@@ -81,13 +84,13 @@ def check_volume_count(system, warn=10, crit=15, **kwargs):
     elif volume_count >= warn and volume_count <= crit:
         msg = ("Warning: Volume count is >=  {} & <= {}. Volume Count = {}"
             .format(warn, crit, volume_count))
-        logger.info(msg)
+        logger.warning(msg)
         print(msg)
         sys.exit(1)
     elif volume_count > crit:
         msg = ("Critical: Volume count is greater than {}. Volume Count = {}".format(
             crit, volume_count))
-        logger.info(msg)
+        logger.error(msg)
         print(msg)
         sys.exit(2)
     else:
@@ -97,8 +100,48 @@ def check_volume_count(system, warn=10, crit=15, **kwargs):
     sys.exit(3)
 
 
+def check_services_status(system, **kwargs):
+    """Check to see if service are in the desired state"""
+    logger = kwargs["logger"]
+    hosts = list(set([host.host_name for host in system.api.hosts.list()]))
+    hosts_agents = dict()
+    hosts_status = dict()
+    services = kwargs['services']
+
+    for host in hosts:
+        # if a hostname contains localhost, we want to avoid trying to connect
+        if 'localhost' in host:
+            continue
+        ssh = ssh_client(host, username="root", password=system.password)
+        with ssh:
+            for service_name, status in services.iteritems():
+                service_status = is_service_in_status(ssh, service_name, status)
+                try:
+                    hosts_agents[host].update({service_name: service_status})
+                except KeyError:
+                    hosts_agents[host] = {service_name: service_status}
+
+        hosts_status[host] = all(hosts_agents[host].values())
+
+    overall_status = all(hosts_status.values())
+
+    if overall_status:  # all true, everything is running
+        msg = ("Ok: all services {} are in the desired state on all hosts".format(services.keys()))
+        logger.info(msg)
+        print(msg)
+        sys.exit(0)
+    else:
+        trouble_hosts = [host for host, status in hosts_status.iteritems() if not status]
+        msg = ("Critical: These hosts don't have all agents in the desired state: {}."
+               "Overall status is {} (where False means not in desired state)"
+               .format(trouble_hosts, hosts_agents))
+        logger.error(msg)
+        print(msg)
+        sys.exit(2)
+
 CHECKS = {
     "vm_count": check_vm_count,
     "keypair_count": check_keypair_count,
-    "volume_count": check_volume_count
+    "volume_count": check_volume_count,
+    "services_status": check_services_status
 }
